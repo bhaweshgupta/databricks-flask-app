900574363e93c4e492f091f061aff2bdfb4d19ae
bgupta <bgupta@pantherxrare.com>
2025-09-08 12:21:50 +0530

    port changed

--- app.py
+++ app.py
@@ -61,4 +61,4 @@ def delete_todo(id):
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=int(os.getenv('PORT', 8080))) 
